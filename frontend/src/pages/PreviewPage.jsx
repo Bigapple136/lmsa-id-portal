ffd270c155e33bc79806aab9f6c79f2ff1073c2b
Yegan <stone.yegan@gmail.com>
2026-03-29 23:06:11 +0000

    Fix useParams import issue

--- frontend/src/pages/PreviewPage.jsx
+++ frontend/src/pages/PreviewPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import IDCardDisplay from '../components/IDCardDisplay'
 import CardCanvas from '../components/CardCanvas'
 import PrintPreviewModal from '../components/PrintPreviewModal'
